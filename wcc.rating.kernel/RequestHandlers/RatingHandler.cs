1098698dc0d1a7ea0a93ebc8bf3ec1d4b01d4f7d
Yev <str.lucky86@gmail.com>
2023-12-03 01:05:20 -0500

    added calculation of rating

--- wcc.rating.kernel/RequestHandlers/RatingHandler.cs
+++ wcc.rating.kernel/RequestHandlers/RatingHandler.cs
@@ -50,16 +50,16 @@ namespace wcc.rating.kernel.RequestHandlers
             rating.ForEach(r => model.Add(_mapper.Map<RatingModel>(r)));
 
             #region Calculate rating
-            List<Game> games = _db.GetGames();
+            List<Game> games = _db.GetGames().OrderBy(e => e.GameId).ToList();
             foreach (var game in games)
             {
                 if (game.HScore == 0 && game.VScore == 0)
                     continue;
 
+                var scores = ScoreHelper.GetBO3Score(game.HScore, game.VScore);
+
                 if (game.GameType == GameType.Individual)
                 {
-                    var scores = ScoreHelper.GetBO3Score(game.HScore, game.VScore);
-
                     var hRating = getAndAddRatingIfMissing(game.HPlayerId, ref model);
 
                     var vRating = getAndAddRatingIfMissing(game.VPlayerId, ref model);
@@ -92,6 +92,86 @@ namespace wcc.rating.kernel.RequestHandlers
 
                     model.First(p => p.PlayerId == GetPlayerIdQuickFix(game.HPlayerId)).Points = hprogress;
                     model.First(p => p.PlayerId == GetPlayerIdQuickFix(game.VPlayerId)).Points = vprogress;
+                }
+                else if (game.GameType == GameType.Teams)
+                {
+                    List<RatingModel> hRatingTeam = new List<RatingModel>();
+                    foreach (var playerId in game.HParticipants)
+                    {
+                        hRatingTeam.Add(getAndAddRatingIfMissing(playerId, ref model));
+                    }
+                    var hRatingAverage = hRatingTeam.Select(r => r.Points).Average();
+
+                    List<RatingModel> vRatingTeam = new List<RatingModel>();
+                    foreach (var playerId in game.VParticipants)
+                    {
+                        vRatingTeam.Add(getAndAddRatingIfMissing(playerId, ref model));
+                    }
+                    var vRatingAverage = vRatingTeam.Select(r => r.Points).Average();
+
+                    var modelOrdered = model.OrderByDescending(p => p.Points).ToList();
+
+                    List<double> hFactorList = new List<double>();
+                    List<int> hPositionList = new List<int>();
+                    foreach (var hRating in hRatingTeam)
+                    {
+                        var hPosition = modelOrdered.IndexOf(hRating);
+                        if (hPosition == -1) hPosition = model.Count + 1;
+                        hPositionList.Add(hPosition);
+                        hFactorList.Add(EloHelper.GetKFactor(hPosition));
+                    }
+                    var hFactor = hFactorList.Average();
+
+                    List<double> vFactorList = new List<double>();
+                    List<int> vPositionList = new List<int>();
+                    foreach (var vRating in vRatingTeam)
+                    {
+                        var vPosition = modelOrdered.IndexOf(vRating);
+                        if (vPosition == -1) vPosition = model.Count + 1;
+                        vPositionList.Add(vPosition);
+                        vFactorList.Add(EloHelper.GetKFactor(vPosition));
+                    }
+                    var vFactor = vFactorList.Average();
+
+                    if (game.IsTechScored)
+                    {
+                        hFactor /= 2.0;
+                        vFactor /= 2.0;
+                    }
+
+                    // kFactor of lower rated player
+                    double kFactorLower =
+                        hPositionList.Average() > vPositionList.Average() ?
+                        hFactor : vFactor;
+
+                    // Rating progress
+                    var newRating = EloHelper.Count(hRatingAverage, vRatingAverage, scores.Item1,
+                        game.HScore >= game.VScore ? hFactor : kFactorLower,
+                        game.VScore >= game.HScore ? vFactor : kFactorLower);
+
+                    var hprogress = Convert.ToInt32(newRating.Item1 - hRatingAverage);
+                    var vprogress = Convert.ToInt32(newRating.Item2 - vRatingAverage);
+
+                    // if team won 1-0 and has regress then set progress equeals 0
+                    if (game.HScore >= game.VScore && hprogress < 0) hprogress /= 5;
+                    if (game.HScore <= game.VScore && hprogress > 0) hprogress /= 5;
+
+                    if (game.VScore >= game.HScore && vprogress < 0) vprogress /= 5;
+                    if (game.VScore <= game.HScore && vprogress > 0) vprogress /= 5;
+
+                    // if lose reduce lost points twice
+                    if (game.HScore >= game.VScore) vprogress /= 2;
+                    if (game.VScore >= game.HScore) hprogress /= 2;
+
+                    foreach (var playerId in game.HParticipants)
+                    {
+                        model.First(p => p.PlayerId == GetPlayerIdQuickFix(playerId)).Points += hprogress;
+                    }
+
+                    foreach (var playerId in game.VParticipants)
+                    {
+                        model.First(p => p.PlayerId == GetPlayerIdQuickFix(playerId)).Points += vprogress;
+                    }
                 }
             }
             #endregion
